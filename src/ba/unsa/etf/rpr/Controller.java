b36f7e285dc02e7e5d7a4e06a2b0cc927b89129a
Emil Fejzagić <efejzagic2@etf.unsa.ba>
2021-12-20 16:25:21 +0100

    Priprema za tutorijal

--- src/ba/unsa/etf/rpr/Controller.java
+++ src/ba/unsa/etf/rpr/Controller.java
@@ -1,11 +1,21 @@
 package ba.unsa.etf.rpr;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+    public ListView lvStudenti;
+    public TextField fldIme;
+    public TextField fldPrezime;
+    public TextField fldMail;
+    public TextField fldUsername;
+    public PasswordField fldPassword;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
