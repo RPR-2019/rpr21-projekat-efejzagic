936877bc9a928162c6820433ea452c307080b1a5
Emil Fejzagić <efejzagic2@etf.unsa.ba>
2021-12-20 16:28:55 +0100

    Implementirana akcija za dugme Kraj

--- src/ba/unsa/etf/rpr/Controller.java
+++ src/ba/unsa/etf/rpr/Controller.java
@@ -1,9 +1,11 @@
 package ba.unsa.etf.rpr;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,5 +21,10 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void actionKraj(ActionEvent actionEvent) {
+        Stage stage = (Stage)fldIme.getScene().getWindow();
+        stage.close();
     }
 }
